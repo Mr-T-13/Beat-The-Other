5521a634f0452ac296c67a06be5f713afc764577
Mr.T <72496013+Mr-T-13@users.noreply.github.com>
2021-01-20 12:10:16 +0100

    Update WebSocketCharacterHandler.java

--- server/GestorCont/src/main/java/com/example/demo/WebSocketCharacterHandler.java
+++ server/GestorCont/src/main/java/com/example/demo/WebSocketCharacterHandler.java
@@ -1,8 +1,42 @@
 package com.example.demo;
 
-import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.*;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class WebSocketCharacterHandler extends TextWebSocketHandler {
 	
+
+	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+	private ObjectMapper mapper = new ObjectMapper();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println("New user: " + session.getId());
+		sessions.put(session.getId(), session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("Session closed: " + session.getId());
+		sessions.remove(session.getId());
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		System.out.println("Message received: " + message.getPayload());
+		JsonNode node = mapper.readTree(message.getPayload());
+		
+		
+	}
+
 
 }
